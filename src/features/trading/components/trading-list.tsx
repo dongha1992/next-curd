eb349099e264a0cbe3b5017e4927d4f091f61bcb
dongha.kim <kamdunga2002@gmail.com>
2025-07-15 22:00:31 +0900

    chore: user.email 변경

--- src/features/trading/components/trading-list.tsx
+++ src/features/trading/components/trading-list.tsx
@@ -4,7 +4,7 @@ const TradingList = () => {
   return (
     <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
       <div className="w-full max-w-[420px] flex gap-x-2">
-        <Placeholder label="No tickets found" />
+        <Placeholder label="게시글이 없습니다." />
       </div>
     </div>
   );
